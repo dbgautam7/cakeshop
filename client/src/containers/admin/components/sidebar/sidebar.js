b5bf3746caaed1c2256e1a6e0f119777af949692
D B Gautam <gautamdb28@gmail.com>
2023-01-28 22:12:41 +0545

    color change feature added

--- client/src/containers/admin/components/sidebar/sidebar.js
+++ client/src/containers/admin/components/sidebar/sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{useState} from 'react'
 import {
   CDBSidebar,
   CDBSidebarContent,
@@ -12,9 +12,15 @@ import { GrUserAdmin } from 'react-icons/gr';
 import { CgProfile } from 'react-icons/cg';
 import { TbDeviceAnalytics } from 'react-icons/tb';
 
-
 const MySidebar = () => {
+  const [selectedColor, setSelectedColor] = useState('grey');
+
+  const handleColorChange=(color)=>{
+      setSelectedColor(color)
+  }
+
   return (
+    <div className="sidebar" style={{backgroundColor: selectedColor}}>
     <div style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}>
       <CDBSidebar textColor="#fff" backgroundColor="#333">
         <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
@@ -25,21 +31,27 @@ const MySidebar = () => {
 
         <CDBSidebarContent className="sidebar-content">
           <CDBSidebarMenu>
+            <p style={{textDecoration:"underline", marginTop:"1opx", marginBottom:"5px"}}>Main</p>
             <NavLink exact to="/adminDashboard" activeClassName="activeClicked">
               <CDBSidebarMenuItem icon="columns"> Dashboard</CDBSidebarMenuItem>
             </NavLink>
+            <p style={{textDecoration:"underline",  marginTop:"1opx", marginBottom:"5px"}}>Lists</p>
             <NavLink exact to="/users" activeClassName="activeClicked">
               <CDBSidebarMenuItem icon="users">Users</CDBSidebarMenuItem>
             </NavLink>
             <NavLink exact to="/analytics" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="chart-line">{<TbDeviceAnalytics />} Analytics</CDBSidebarMenuItem>
+              <CDBSidebarMenuItem icon="chart-line">{<TbDeviceAnalytics />} Products</CDBSidebarMenuItem>
             </NavLink>
             <NavLink exact to="/analytics" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="chart-line">{<TbDeviceAnalytics />} Delivery</CDBSidebarMenuItem>
+              <CDBSidebarMenuItem icon="chart-line">{<TbDeviceAnalytics />} Orders</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/analytics" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="chart-line">{<TbDeviceAnalytics />} Users</CDBSidebarMenuItem>
             </NavLink>
             <NavLink exact to="/analytics" activeClassName="activeClicked">
               <CDBSidebarMenuItem icon="chart-line">{<TbDeviceAnalytics />} Notifications</CDBSidebarMenuItem>
             </NavLink>
+            <p style={{textDecoration:"underline", marginTop:"1opx", marginBottom:"5px"}}>Additional Features</p>
             <NavLink exact to="/analytics" activeClassName="activeClicked">
               <CDBSidebarMenuItem icon="chart-line">{<TbDeviceAnalytics />} Settings</CDBSidebarMenuItem>
             </NavLink>
@@ -49,20 +61,26 @@ const MySidebar = () => {
             <NavLink exact to="/analytics" activeClassName="activeClicked">
               <CDBSidebarMenuItem icon="chart-line">{<TbDeviceAnalytics />} Logout</CDBSidebarMenuItem>
             </NavLink>
-
           </CDBSidebarMenu>
         </CDBSidebarContent>
-
+        
+    <div className="color-options">
+      <button className="color-option" onClick={() => handleColorChange('white')}>White</button>
+      <button className="color-option" onClick={() => handleColorChange('green')}>Green</button>
+    
+    </div>
         <CDBSidebarFooter style={{ textAlign: 'center' }}>
           <div
             style={{
               padding: '20px 5px',
+              marginTop:"80px"
             }}
           >
             Copyright @2022 All Rights Reserved.
           </div>
         </CDBSidebarFooter>
       </CDBSidebar>
+    </div>
     </div>
   );
 };
